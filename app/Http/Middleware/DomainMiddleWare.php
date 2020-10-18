87b968336d2649bc593e6e7eb458619faa5f19cd
noaman kazi <noaman.kazi@gmail.com>
2020-10-18 17:43:21 +0400

    href lang, sitemap, favicon, seo

--- app/Http/Middleware/DomainMiddleWare.php
+++ app/Http/Middleware/DomainMiddleWare.php
@@ -51,5 +51,6 @@ class DomainMiddleWare
         return $next($request)->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+            
     }
 }
--- resources/views/layout/v2/mainlayout.blade.php
+++ resources/views/layout/v2/mainlayout.blade.php
@@ -512,19 +512,6 @@ $reviewername=randomName();
 </script>
 
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5e1068e57e39ea1242a30129/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
 
 
 <script src='https://www.google.com/recaptcha/api.js'></script>
@@ -1131,5 +1118,22 @@ return new Promise(function (resolve, reject) {
 
     </script>
    
+
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e1068e57e39ea1242a30129/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+
   </body>
 </html>
