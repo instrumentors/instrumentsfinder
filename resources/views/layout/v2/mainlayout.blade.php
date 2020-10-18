058bab9a2b754a6d253d8292cddbf64de629eb67
noaman kazi <noaman.kazi@gmail.com>
2020-10-18 19:11:30 +0400

    href lang, sitemap, favicon, seo

--- resources/views/layout/v2/mainlayout.blade.php
+++ resources/views/layout/v2/mainlayout.blade.php
@@ -525,6 +525,7 @@ s1.async=true;
 s1.src='https://embed.tawk.to/5e1068e57e39ea1242a30129/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
+
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
